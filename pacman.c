d7a669fc3bb78ef4e9f60495a788f906721fa101
unknown <persiking123@gmail.com>
2025-01-31 13:01:15 +0330

    save game

--- pacman.c
+++ pacman.c
@@ -1,201 +1,215 @@
-// https://www.geeksforgeeks.org/pacman-game-in-c/
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// Pacman Game in C language 
-#include <conio.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
+#define WIDTH 40
+#define HEIGHT 20
+#define PACMAN 'C'
+#define WALL '#'
+#define FOOD '.'
+#define EMPTY ' '
+#define DEMON 'X'
 
-// All the elements to be used 
-// Declared here 
-#define WIDTH 40 
-#define HEIGHT 20 
-#define PACMAN 'C' 
-#define WALL '#' 
-#define FOOD '.' 
-#define EMPTY ' ' 
-#define DEMON 'X' 
+int res = 0;
+int score = 0;
+int pacman_x, pacman_y;
+char board[HEIGHT][WIDTH];
+int food = 0;
+int curr = 0;
 
-// Global Variables are 
-// Declared here 
-int res = 0; 
-int score = 0; 
-int pacman_x, pacman_y; 
-char board[HEIGHT][WIDTH]; 
-int food = 0; 
-int curr = 0; 
-void initialize() 
-{ 
-	// Putting Walls as boundary in the Game 
-	for (int i = 0; i < HEIGHT; i++) { 
-		for (int j = 0; j < WIDTH; j++) { 
-			if (i == 0 || j == WIDTH - 1 || j == 0 
-				|| i == HEIGHT - 1) { 
-				board[i][j] = WALL; 
-			} 
-			else
-				board[i][j] = EMPTY; 
-		} 
-	} 
+void save_game() {
+    FILE *file = fopen("D:/Games/saman.txt", "w");
+    if (file != NULL) {
+        // ذخیره اطلاعات بازی
+        fprintf(file, "%d %d %d %d %d\n", pacman_x, pacman_y, score, food, curr);
 
-	// Putting Walls inside the Game 
-	int count = 50; 
-	while (count != 0) { 
-		int i = (rand() % (HEIGHT + 1)); 
-		int j = (rand() % (WIDTH + 1)); 
+        // ذخیره وضعیت صفحه بازی
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                fputc(board[i][j], file);
+            }
+        }
+        fclose(file);
+    }
+}
 
-		if (board[i][j] != WALL && board[i][j] != PACMAN) { 
-			board[i][j] = WALL; 
-			count--; 
-		} 
-	} 
+int load_game() {
+    FILE *file = fopen("D:/Games/saman.txt", "r");
+    if (file != NULL) {
+        // بارگذاری اطلاعات بازی
+        fscanf(file, "%d %d %d %d %d\n", &pacman_x, &pacman_y, &score, &food, &curr);
 
-	int val = 5; 
-	while (val--) { 
-		int row = (rand() % (HEIGHT + 1)); 
-		for (int j = 3; j < WIDTH - 3; j++) { 
-			if (board[row][j] != WALL 
-				&& board[row][j] != PACMAN) { 
-				board[row][j] = WALL; 
-			} 
-		} 
-	} 
+        // بارگذاری وضعیت صفحه بازی
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                board[i][j] = fgetc(file);
+            }
+        }
 
-	// Putting Demons in the Game 
-	count = 10; 
-	while (count != 0) { 
-		int i = (rand() % (HEIGHT + 1)); 
-		int j = (rand() % (WIDTH + 1)); 
+        fclose(file);
+        return 1;
+    }
+    return 0;
+}
 
-		if (board[i][j] != WALL && board[i][j] != PACMAN) { 
-			board[i][j] = DEMON; 
-			count--; 
-		} 
-	} 
+void initialize() {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (i == 0 || j == WIDTH - 1 || j == 0 || i == HEIGHT - 1) {
+                board[i][j] = WALL;
+            } else {
+                board[i][j] = EMPTY;
+            }
+        }
+    }
 
-	// Cursor at Center 
-	pacman_x = WIDTH / 2; 
-	pacman_y = HEIGHT / 2; 
-	board[pacman_y][pacman_x] = PACMAN; 
+    int count = 50;
+    while (count != 0) {
+        int i = (rand() % (HEIGHT + 1));
+        int j = (rand() % (WIDTH + 1));
 
-	// Points Placed 
-	for (int i = 0; i < HEIGHT; i++) { 
-		for (int j = 0; j < WIDTH; j++) { 
-			if (i % 2 == 0 && j % 2 == 0 
-				&& board[i][j] != WALL 
-				&& board[i][j] != DEMON 
-				&& board[i][j] != PACMAN) { 
+        if (board[i][j] != WALL && board[i][j] != PACMAN) {
+            board[i][j] = WALL;
+            count--;
+        }
+    }
 
-				board[i][j] = FOOD; 
-				food++; 
-			} 
-		} 
-	} 
-} 
+    int val = 5;
+    while (val--) {
+        int row = (rand() % (HEIGHT + 1));
+        for (int j = 3; j < WIDTH - 3; j++) {
+            if (board[row][j] != WALL && board[row][j] != PACMAN) {
+                board[row][j] = WALL;
+            }
+        }
+    }
 
-void draw() 
-{ 
-	// Clear screen 
-	system("cls"); 
+    count = 10;
+    while (count != 0) {
+        int i = (rand() % (HEIGHT + 1));
+        int j = (rand() % (WIDTH + 1));
 
-	// Drawing All the elements in the screen 
-	for (int i = 0; i < HEIGHT; i++) { 
-		for (int j = 0; j < WIDTH; j++) { 
-			printf("%c", board[i][j]); 
-		} 
-		printf("\n"); 
-	} 
-	printf("Score: %d\n", score); 
-} 
+        if (board[i][j] != WALL && board[i][j] != PACMAN) {
+            board[i][j] = DEMON;
+            count--;
+        }
+    }
 
-// Function enables to move the Cursor 
-void move(int move_x, int move_y) 
-{ 
-	int x = pacman_x + move_x; 
-	int y = pacman_y + move_y; 
+    pacman_x = WIDTH / 2;
+    pacman_y = HEIGHT / 2;
+    board[pacman_y][pacman_x] = PACMAN;
 
-	if (board[y][x] != WALL) { 
-		if (board[y][x] == FOOD) { 
-			score++; 
-			food--; 
-			curr++; 
-			if (food == 0) { 
-				res = 2; 
-				return; 
-			} 
-		} 
-		else if (board[y][x] == DEMON) { 
-			res = 1; 
-		} 
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (i % 2 == 0 && j % 2 == 0 && board[i][j] != WALL && board[i][j] != DEMON && board[i][j] != PACMAN) {
+                board[i][j] = FOOD;
+                food++;
+            }
+        }
+    }
+}
 
-		board[pacman_y][pacman_x] = EMPTY; 
-		pacman_x = x; 
-		pacman_y = y; 
-		board[pacman_y][pacman_x] = PACMAN; 
-	} 
-} 
+void draw() {
+    system("cls");
 
-// Main Function 
-int main() 
-{ 
-	initialize(); 
-	char ch; 
-	food -= 35; 
-	int totalFood = food; 
-	// Instructions to Play 
-	printf(" Use buttons for w(up), a(left) , d(right) and "
-		"s(down)\nAlso, Press q for quit\n"); 
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            printf("%c", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Score: %d\n", score);
+}
 
-	printf("Enter Y to continue: \n"); 
+void move(int move_x, int move_y) {
+    int x = pacman_x + move_x;
+    int y = pacman_y + move_y;
 
-	ch = getch(); 
-	if (ch != 'Y' && ch != 'y') { 
-		printf("Exit Game! "); 
-		return 1; 
-	} 
+    if (board[y][x] != WALL) {
+        if (board[y][x] == FOOD) {
+            score++;
+            food--;
+            curr++;
+            if (food == 0) {
+                res = 2;
+                return;
+            }
+        } else if (board[y][x] == DEMON) {
+            res = 1;
+        }
 
-	while (1) { 
-		draw(); 
-		printf("Total Food count: %d\n", totalFood); 
-		printf("Total Food eaten: %d\n", curr); 
-		if (res == 1) { 
-			// Clear screen 
-			system("cls"); 
-			printf("Game Over! Dead by Demon\n Your Score: "
-				"%d\n", 
-				score); 
-			return 1; 
-		} 
+        board[pacman_y][pacman_x] = EMPTY;
+        pacman_x = x;
+        pacman_y = y;
+        board[pacman_y][pacman_x] = PACMAN;
+    }
+}
 
-		if (res == 2) { 
-			// Clear screen 
-			system("cls"); 
-			printf("You Win! \n Your Score: %d\n", score); 
-			return 1; 
-		} 
+int main() {
+    char ch;
+    int load = 0;
 
-		// Taking the Input from the user 
-		ch = getch(); 
+    printf("Do you want to load the previous game? (Y/N): ");
+    ch = getch();
 
-		// Moving According to the 
-		// input character 
-		switch (ch) { 
-		case 'w': 
-			move(0, -1); 
-			break; 
-		case 's': 
-			move(0, 1); 
-			break; 
-		case 'a': 
-			move(-1, 0); 
-			break; 
-		case 'd': 
-			move(1, 0); 
-			break; 
-		case 'q': 
-			printf("Game Over! Your Score: %d\n", score); 
-			return 0; 
-		} 
-	} 
+    if (ch == 'Y' || ch == 'y') {
+        load = load_game();
+    }
 
-	return 0; 
+    if (!load) {
+        initialize();
+    }
+
+    food -= 35;
+    int totalFood = food;
+
+    printf("Use buttons for w(up), a(left), d(right), and s(down)\nAlso, Press q for quit\n");
+    printf("Enter Y to continue: \n");
+
+    ch = getch();
+    if (ch != 'Y' && ch != 'y') {
+        printf("Exit Game! ");
+        return 1;
+    }
+
+    while (1) {
+        draw();
+        printf("Total Food count: %d\n", totalFood);
+        printf("Total Food eaten: %d\n", curr);
+
+        if (res == 1) {
+            system("cls");
+            printf("Game Over! Dead by Demon\nYour Score: %d\n", score);
+            return 1;
+        }
+
+        if (res == 2) {
+            system("cls");
+            printf("You Win!\nYour Score: %d\n", score);
+            return 1;
+        }
+
+        ch = getch();
+
+        switch (ch) {
+            case 'w':
+                move(0, -1);
+                break;
+            case 's':
+                move(0, 1);
+                break;
+            case 'a':
+                move(-1, 0);
+                break;
+            case 'd':
+                move(1, 0);
+                break;
+            case 'q':
+                printf("Game Over! Your Score: %d\n", score);
+                save_game();
+                return 0;
+        }
+    }
+
+    return 0;
 }
